92fc0b047846601a8bdffbad5d4080a87dfd4f88
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2024-09-03 09:54:11 +0600

    update project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Products } from '../../types';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  constructor(private productsService: ProductsService) {}
 
+  ngOnInit() {
+    this.productsService
+      .getProducts('http://localhost:3000/cloths', { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        console.log(products.items);
+      });
+  }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PaginationParams, Products } from '../../types';
+import { ApiService } from './api.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-
-  constructor() { }
+  constructor(private apiService: ApiService) {}
+  getProducts = (
+    url: string,
+    params: PaginationParams
+  ): Observable<Products> => {
+    return this.apiService.get(url, { params, responseType: 'json' });
+  };
 }
