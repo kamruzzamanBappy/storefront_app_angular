89b2a9e5710ea0cb7df50191c82ba911bba8d5a8
kamruzzamanBappy <kamruzzamanbappy13@gmail.com>
2024-09-04 22:32:02 +0600

    product page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,24 @@
 import { Component } from '@angular/core';
-import { Products } from '../../types';
+import { Product, Products } from '../../types';
+import { ProductComponent } from '../components/product/product.component';
 import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
+  products: Product[] = [];
   ngOnInit() {
     this.productsService
       .getProducts('http://localhost:3000/cloths', { page: 0, perPage: 5 })
       .subscribe((products: Products) => {
-        console.log(products.items);
+        this.products = products.items;
       });
   }
 }
